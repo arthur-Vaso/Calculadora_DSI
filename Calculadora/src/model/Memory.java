d6a5573c7dfb5ac98083deb5fa82c8818dc42797
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-09-29 19:59:22 -0300

    Classe memory todos os double foram trocados para String

--- Calculadora/src/model/Memory.java
+++ Calculadora/src/model/Memory.java
@@ -1,36 +1,36 @@
 package model;
 
 public class Memory {
-	private double firstNumber = 0;
-	private double secondNumber = 0;
-	private double result;
+	private String firstNumber;
+	private String secondNumber;
+	private String result;
 	private String operator;
 	
 	public Memory() {
 		
 	}
 
-	public double getFirstNumber() {
+	public String getFirstNumber() {
 		return firstNumber;
 	}
 
-	public void setFirstNumber(double firstNumber) {
+	public void setFirstNumber(String firstNumber) {
 		this.firstNumber = firstNumber;
 	}
 
-	public double getSecondNumber() {
+	public String getSecondNumber() {
 		return secondNumber;
 	}
 
-	public void setSecondNumber(double secondNumber) {
+	public void setSecondNumber(String secondNumber) {
 		this.secondNumber = secondNumber;
 	}
 
-	public double getresult() {
+	public String getresult() {
 		return result;
 	}
 
-	public void setresult(double result) {
+	public void setresult(String result) {
 		this.result = result;
 	}
 
